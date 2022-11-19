f7ad345321bc5ed265823e869f2cc1a5ef08b0aa
Ivan Voronin <ViaVoronin@yandex.ru>
2022-11-19 12:54:01 +0300

    add 2 class

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    // Ошибка - деление на ноль. Код не работиал и выкидывал исключение ArithmeticException.
+    // Не совсем понял, какое решение требуется по задаче. Поменя местами числитель и знаменатель
+    BinaryOperator<Integer> devide = (x, y) -> y > 0 ? x / y : y / x;
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+}
